8845d2677a630c82489213e08db889fa3ef7d30b
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-07-05 17:47:05 -0400

    Added all basic functions. schedule allows process to be redone

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import random
 
-print(random.randint(0,2))
-
 def destination():
-    destinations=["Paris"]
+    destinations=["Adirondack Mountains","New York City","Albany"]
+    return destinations[random.randint(0,2)]
 
 def restaurant():
     restaurants=["Applebees","Jollibee","NY Pizza"]
+    return restaurants[random.randint(0,2)]    
 
 def transport():
-    x=5
+    transports=["Bus","Car","Train"]
+    return transports[random.randint(0,2)]
 
 def entertainment():
-    y=5
+    entertainments=["Theatre","Water Park","Fair"]
+    return entertainments[random.randint(0,2)]
+
+def schedule():
+    print("Let's go through your day to determine what you'll be doing!")
+    dest = destination ()
+    choice = input(f"Your destination is {dest}! Is this okay (y/n): ")
+    if (choice != "y"):
+        dest = destination ()
+        choice = (f"Your destination is {dest}! Is this okay (y/n): ")
+    
+
+
+print("Welcome to your Day Planner!")
+schedule()
