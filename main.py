1e064dd8d41426dc773233ad9c4ff2179695a335
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-07-05 17:03:26 -0400

    Defined variables and Random example for reference in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+print(random.randint(0,2))
+
+def destination():
+    destinations=["Paris"]
+
+def restaurant():
+    restaurants=["Applebees","Jollibee","NY Pizza"]
+
+def transport():
+    x=5
+
+def entertainment():
+    y=5
